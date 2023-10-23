7a9332baa3205f3f8f5bf8bec5d9f1f8f084694f
zhiyongcao <zhiyongcao@emotibot.com>
2023-10-23 11:10:49 +0800

    add python

---
+++ source/tips/python/python_demo/chapter2/name_hello.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+message = "Hello " + name.title() + ", would you like to learn some Python today?"
+print(message)
+print(name.lower())
+print(name.upper())
+print(name.title())
---
+++ source/tips/python/python_demo/chapter6/dict_demo.py
@@ -0,0 +1,6 @@
+jiao = {"last_name": "jiao", "first_name": "jiao", "age": 18, "city": "shanghai"}
+print(jiao)
+
+for key, value in jiao.items():
+    print("\nKey: " + key)
+    print("Value: " + str(value))
